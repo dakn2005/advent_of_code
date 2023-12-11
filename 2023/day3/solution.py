5b3ad223de5b14f43c2152241984b46da0a69946
David K. Njuguna <dakn2005@gmail.com>
2023-12-11 16:18:56 +0300

    Day 3 Done - issues were with edge cases!

--- 2023/day3/solution.py
+++ 2023/day3/solution.py
@@ -1,5 +1,6 @@
 import re
 import sys         
+import numpy as np
 
 # sys.path.append('2023/')
 
@@ -19,68 +20,133 @@ class Solution:
         lines = [str(l).replace('\n', '') for l in lines]
 
         return lines
+    
+    def lineBuilder(self, strarr):
+        temparr = []
+        idx = 0
+        theN = []
 
-    def getLineData(self, lines):
-        out=[]
+        while idx <= len(strarr):
+            if idx == len(strarr):
+                if len(theN) > 0:
+                    temparr.append(''.join(theN))
+                    theN.clear()
+                
+                break
+
+            c = strarr[idx]
+
+            if str(c).isdigit():
+                theN.append(c.strip())
+            else:
+                if len(theN) > 0:
+                    temparr.append(''.join(theN))
+                    theN.clear()
+                temparr.append(c)
+
+            idx += 1
+
+        return temparr
+
+    def getLineData_II(self, lines):
+        out = []
         meta: dict = {}
-
-        for i, line  in enumerate(lines):
-            strarr = list(line)
-            temparr, theN = [], []
-            idx=0
-
-            while idx <= len(strarr):
-                if idx == len(strarr):
-                    if len(theN) > 0:
-                        temparr.append(''.join(theN))
-                        theN.clear()
-                    
-                    break
-
-                c = strarr[idx]
-
-                if str(c).isdigit():
-                    theN.append(c.strip())
-                else:
-                    if len(theN) > 0:
-                        temparr.append(''.join(theN))
-                        theN.clear()
-                    temparr.append(c)
-
-                idx+=1
-
-            # temp1 = str(line).split('.')
+        
+        for i,line in enumerate(lines):
+            lineArr = list(line)
+            lineNumbersSet = set([s for s in self.lineBuilder(list(line)) if s.isdigit() ])
+            tempN = ''
+            j = 0
             meta[i] = []
 
-            cnter=0
-            while cnter < len(temparr):
-                c = temparr[cnter]
+            while j <= len(lineArr):
+                c = ''
+                if j < len(lineArr):
+                    c = lineArr[j]
+
                 if str(c).isdigit():
-                    dl = len(c)
-                    meta[i].append({
-                        'digit': c,
-                        'span': (cnter, cnter+dl)
-                    })
-                    cnter+=dl
-                cnter+=1
+                    digitEndIdx = j
+                    tempN+=c
+                elif not str(c).isdigit() and len(tempN) > 0:
+                    if tempN in lineNumbersSet:
+                        endIdx = j
 
-            out.append(list(line))
-            
-            for s in list(line):
-                if not s.isdigit() and s != '.':
-                    self.symbols.add(s)
+                        meta[i].append({
+                            'digit': tempN,
+                            'span': (digitEndIdx - len(tempN) + 1, endIdx)
+                        })
 
+                        tempN = ''
+                j+=1
+
+            out.append(lineArr)
         
         return out, meta
+
+    # TODO: deprecated spanning logic off, need to FIX
+    # def getLineData(self, lines):
+    #     out=[]
+    #     meta: dict = {}
+
+    #     for i, line  in enumerate(lines):
+    #         strarr = list(line)
+    #         temparr, theN = [], []
+    #         idx=0
+
+    #         while idx <= len(strarr):
+    #             if idx == len(strarr):
+    #                 if len(theN) > 0:
+    #                     temparr.append(''.join(theN))
+    #                     theN.clear()
+                    
+    #                 break
+
+    #             c = strarr[idx]
+
+    #             if str(c).isdigit():
+    #                 theN.append(c.strip())
+    #             else:
+    #                 if len(theN) > 0:
+    #                     temparr.append(''.join(theN))
+    #                     theN.clear()
+    #                 temparr.append(c)
+
+    #             idx+=1
+
+    #         # temp1 = str(line).split('.')
+    #         meta[i] = []
+
+    #         cnter=0
+    #         while cnter < len(temparr):
+    #             c = temparr[cnter]
+    #             if str(c).isdigit():
+    #                 dl = len(c)
+    #                 meta[i].append({
+    #                     'digit': c,
+    #                     'span': (cnter, cnter+dl)
+    #                 })
+    #                 cnter+=dl
+    #             cnter+=1
+
+    #         out.append(list(line))
+            
+    #         for s in list(line):
+    #             if not s.isdigit() and s != '.':
+    #                 self.symbols.add(s)
+
+        
+    #     return out, meta
 
     def schematic(self, arr):
         if self.extf:
             arr = self.getLines()
 
-        lines, meta = self.getLineData(arr)
+        lines, meta = self.getLineData_II(arr)
         visited = set()
-
+        modVisited = []
+        
         def dfs(row, col):
+            out = []
             vR,vC = len(lines), len(lines[0])
             directions = [
                 [-1,-1],
@@ -97,15 +163,17 @@ class Solution:
                 R, C = row + r, col + c
                 if R < vR and C < vC and str(lines[R][C]).isdigit():
                     if (R,C) not in visited:
+                        # TODO: 
                         visited.add((R,C))
+                        out.append((R,C))
                         # out.append((R,C))
 
-            # return visited
+            return out
         
-        def getNums():
+        def getNums(tembelea):
             out=[]
             spanSet = set()
-            for (r,c) in visited:
+            for (r,c) in tembelea:
                 if r < 0: continue
                 tarr = meta[r]
                 for obj in tarr:
@@ -115,16 +183,24 @@ class Solution:
                         spanSet.add((r, spanStart,spanEnd))
 
             return out
-
+        
+       
         for i, chars in enumerate(lines):
             for j,char in enumerate(chars):
                 if not char == '.' and not char.isdigit():
-                   dfs(i,j)
+                   modVisited.append(dfs(i,j))
         
         visited = list(sorted(visited))
-        pNums = [int(n) for n in getNums()]
-        # pNums.sort()
-        return sum(pNums)
+        partNums = [int(n) for n in getNums(visited)] #part I
+
+        partProdSum = []
+        for subarr in modVisited:
+            prodArr = getNums(subarr)
+            if len(prodArr) == 2:
+                prod = np.prod([int(n) for n in prodArr])
+                partProdSum.append(prod)
+
+        return sum(partNums), sum(partProdSum)
                    
 
 test1 = [
@@ -225,10 +301,8 @@ test23 = [
     '......*.',
 ]
 
-
-
-t = Solution(False)
-# print(t.schematic(test1))
+t = Solution(True)
+print(t.schematic(test1))
 # print(t.schematic(test15)) #edge case here
 # print(t.schematic(test16)) #edge case here
 # print(t.schematic(test17)) #edge case here
@@ -236,5 +310,5 @@ t = Solution(False)
 # print(t.schematic(test19)) #edge case here
 # print(t.schematic(test20)) #edge case here
 # print(t.schematic(test21)) #edge case here
-print(t.schematic(test22)) #edge case here
+# print(t.schematic(test22)) #edge case here
 # print(t.schematic(test23)) #edge case here
